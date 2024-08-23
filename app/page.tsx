205dc6d78e2cf26ad5a74edd8f8a4d6e4827094a
c4n1sl4v <cmihaylov@gmail.com>
2024-08-23 01:21:47 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
           >
             By{" "}
             <Image
-              src="https://github.com/c4n1sl4v/default-project/blob/c5e05cb4ac9b2d7c59c3e8171b928492f3dce078/public/vercel.svg"
+              src="https://raw.githubusercontent.com/c4n1sl4v/default-project/c5e05cb4ac9b2d7c59c3e8171b928492f3dce078/public/vercel.svg"
               alt="Vercel Logo"
               className="dark:invert"
               width={100}
@@ -31,7 +31,7 @@ export default function Home() {
       <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="https://github.com/c4n1sl4v/default-project/blob/c5e05cb4ac9b2d7c59c3e8171b928492f3dce078/public/next.svg"
+          src="https://raw.githubusercontent.com/c4n1sl4v/default-project/575c5f94db0cafa87ca8c2a0eb87b61191bc257e/public/next.svg"
           alt="Next.js Logo"
           width={180}
           height={37}
