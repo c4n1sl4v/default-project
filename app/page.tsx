a5005b4dbbcd7916febd24288f691378630d4800
c4n1sl4v <cmihaylov@gmail.com>
2024-08-23 01:40:03 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <Image
     className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-    src="/0_dCkBR3Q3lkGMKeAh.png"
+    src="/default-project/0_dCkBR3Q3lkGMKeAh.png"
     alt="Next.js Logo"
     width={180}
     height={37}
