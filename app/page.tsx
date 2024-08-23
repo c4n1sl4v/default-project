d95eaafb6084046555dec494068782fb4600d12e
c4n1sl4v <cmihaylov@gmail.com>
2024-08-23 01:44:26 -0400

    modified:   app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
+    <div>
     <Image
     className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
     src="/default-project/0_dCkBR3Q3lkGMKeAh.png"
@@ -10,5 +11,8 @@ export default function Home() {
     height={37}
     priority
   />
+  <h1>Hello!</h1>
+    </div>
+
   );
 }
